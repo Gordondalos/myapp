fd3cac290c4befed72d31f05c208ab1b24b807c6
Gordondalos <gordondalos@gmail.com>
2016-09-18 19:17:33 +0600

    frontend base finish

--- src/js/project/module/myApp.js
+++ src/js/project/module/myApp.js
@@ -12,26 +12,53 @@ myApp.directive('backImg', function(){
 });
 
 
-myApp.controller("PhotoController", ["$scope",'$http', function ($scope,$http) {
+myApp.controller("PhotoController", ["$scope",'$http', function ($scope, $http) {
 
-    $scope.addPhoto =  function (q) {
-        console.log(q);
+    $scope.send =  function () {
+        var that = this;
+        var arr_img = $('img').parent();
+        var arr_src = [];
+        arr_img.each(function(index,item){
+            arr_src.push($(item).attr('href'));
+        });
 
+        that.q.photos = JSON.stringify(arr_src);
+        that.q.data_send = new Date();
 
+        // console.log(JSON.stringify($scope.q));
+        // $http.post("ajax.php",  JSON.stringify($scope.q)).success(function (answer) {
+        //     console.log(answer);
+        //
+        // });
+
+        $.post("ajax.php",
+            {
+                add: that.q,
+            },
+            onAjaxSuccess
+        );
+
+        function onAjaxSuccess(data)
+        {
+            console.log(data);
+        }
     };
 
-    $scope.q =     {
-        photos: [
-            {
-                name: "Первая фотка",
-                src: 'http://wnature.net/pictures/2014/02/243/krasivaya-polosataya-tropicheskaya-rybka.jpg'
-            }
-        ],
-        client: 'Василий Теркин',
-        date: '2016-09-01',
-        number: 200,
-        partner: 13,
-        shop: 'amason.com'
+    $scope.v = function () {
+        var loaction = window.location.href;
+        var res = loaction.split("q=")[1];
+        if(res){
+            return res;
+        }
+    }();
+
+    $scope.q =  {
+        photos: '',
+        client: '',
+        date: '',
+        number: 0,
+        partner: 0,
+        shop: ''
 
     };
 
@@ -70,10 +97,7 @@ myApp.controller("PhotoController", ["$scope",'$http', function ($scope,$http) {
                         partner: 16,
                         shop: 'watch.com'
                     }
-
     ];
-
-
 
 
 
